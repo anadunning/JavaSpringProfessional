9d6ff3254d0a168222b1346f7aac787f1fab4fba
anadunning <anakpedrosa@hotmail.com>
2025-02-01 21:39:54 -0300

    Basic Java example using components and Dependency Injection

---
+++ salary-java/src/app/Program.java
@@ -0,0 +1,35 @@
+package app;
+
+import entities.Employee;
+import services.PensionService;
+import services.SalaryService;
+import services.TaxService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.UK);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        String name = sc.nextLine();
+        System.out.print("Salario bruto: ");
+        double grossSalary = sc.nextDouble();
+
+        Employee employee = new Employee(name, grossSalary);
+        TaxService taxService = new TaxService();
+        PensionService pensionService = new PensionService();
+
+        SalaryService salaryService = new SalaryService(taxService, pensionService);
+
+        double netSalary = salaryService.netSalary(employee);
+
+        System.out.printf("Salario liquido: %.2f%n ", netSalary );
+
+        sc.close();
+    }
+}
