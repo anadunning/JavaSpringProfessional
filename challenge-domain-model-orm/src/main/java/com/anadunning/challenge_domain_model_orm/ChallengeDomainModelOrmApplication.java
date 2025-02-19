32398f350362e98ba784b49472841398135a6b5a
anadunning <anakpedrosa@hotmail.com>
2025-02-19 10:58:47 -0300

    2nd challenge from JavaSpringProfessional course - Domain Model and ORM

---
+++ challenge-domain-model-orm/src/main/java/com/anadunning/challenge_domain_model_orm/ChallengeDomainModelOrmApplication.java
@@ -0,0 +1,13 @@
+package com.anadunning.challenge_domain_model_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDomainModelOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDomainModelOrmApplication.class, args);
+	}
+
+}
---
+++ challenge-domain-model-orm/src/test/java/com/anadunning/challenge_domain_model_orm/ChallengeDomainModelOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anadunning.challenge_domain_model_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDomainModelOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
