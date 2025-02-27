84c42df91cc3ad932c2678d258c8817e3cd0bb75
anadunning <anakpedrosa@hotmail.com>
2025-02-26 21:27:00 -0300

    GET Product by Id

---
+++ dscommerce/src/main/java/com/anadunning/dscommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.anadunning.dscommerce.services;
+
+import com.anadunning.dscommerce.dto.ProductDTO;
+import com.anadunning.dscommerce.entities.Product;
+import com.anadunning.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
+}
