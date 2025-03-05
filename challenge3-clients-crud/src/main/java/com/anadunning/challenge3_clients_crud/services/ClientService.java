d8c56d19f8905ea436fd2d1352f9425dd650f68c
anadunning <anakpedrosa@hotmail.com>
2025-03-04 23:14:11 -0300

    3rd challenge - Clients CRUD - findAll

--- challenge3-clients-crud/src/main/java/com/anadunning/challenge3_clients_crud/services/ClientService.java
+++ challenge3-clients-crud/src/main/java/com/anadunning/challenge3_clients_crud/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,17 +16,16 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-//    public ClientDTO findById(Long id) {
-//        Optional<Client> result = repository.findById(id);
-//        Client client = result.get();
-//        ClientDTO dto = new ClientDTO(client);
-//        return dto;
-//    }
-
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ClientDTO> findAll() {
+        List<Client> result = repository.findAll();
+        return result.stream().map(x -> new ClientDTO(x)).toList();
     }
 
 }
